32e0ce02d8d7dd234fd01445aa3a9dab19db3495
melanie <mel.92527@gmail.com>
2020-02-23 00:29:19 +0800

    Initial commit

---
+++ dist/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class App {
+    constructor() {
+        console.log("Hello app!");
+    }
+}
+exports.App = App;
---
+++ dist/Main.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __importStar(require("react"));
+class Main extends React.Component {
+    render() {
+        return (React.createElement("div", null,
+            React.createElement("h1", null, "Hello main")));
+    }
+}
+exports.Main = Main;
